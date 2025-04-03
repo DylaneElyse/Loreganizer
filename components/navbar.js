ebee0e5d311b90d349a1df9a142e1bf692146de6
Dylane Cunningham <155758209+DylaneElyse@users.noreply.github.com>
2025-04-03 15:52:26 -0600

    Update pages

--- components/navbar.js
+++ components/navbar.js
@@ -6,24 +6,26 @@ export default function Navbar() {
 
   if (user) {
     return (
-      <div>
+    <div>
       <div className="navbar bg-accent">
-      <Link href="/" className="flex-1">
-        <h1 className="btn btn-ghost text-xl">Loreganizer</h1>
-      </Link>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-        <li><Link href="/campaign">Campaign</Link></li>
-        <li>
-          <details>
-          <summary>Account</summary>
-          <ul className="bg-base-100 rounded-t-none p-2">
-            <li onClick={firebaseSignOut} className="cursor-pointer">Logout</li>
+        <div className="flex-1 p-3">
+          <h1 className="text-xl font-semibold">Loreganizer</h1>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/campaign-page">Campaign</Link></li>
+            <li>
+              <details>
+                <summary>Account</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><Link href="/dashboard-page">Dashboard</Link></li>
+                  <li onClick={firebaseSignOut} className="cursor-pointer"><Link href="/">Logout</Link></li>
+                </ul>
+              </details>
+            </li>
           </ul>
-          </details>
-        </li>
-        </ul>
-      </div>
+        </div>
       </div>
     </div>
     )
@@ -33,11 +35,12 @@ export default function Navbar() {
     return (
     <div>
       <div className="navbar bg-accent">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Loreganizer</a>
+        <div className="flex-1 p-3">
+          <h1 className="text-xl font-semibold">Loreganizer</h1>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
+            <li><Link href="/">Home</Link></li>
             <li><Link href="/login-page">Login</Link></li>
             <li>
               <Link href="/register-page">Register</Link>
